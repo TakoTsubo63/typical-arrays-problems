e4c10e1f3cb8911d625016deff82242a55535108
Sergey Koval <sergeyoveil@gmail.com>
2021-03-09 13:01:37 +0400

    docs: changed index.js & finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+ let f = [];
+ if (array == undefined || array.length == 0) {
+     return f = 0 ;
+ }
+ for (let i = 0; i < array.length; i++) {
+    if (array[i] < f){
+        f = array[i];
+    }
+ }
+ return f;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let f = [];
+    if (array == undefined || array.length == 0) {
+        return f = 0 ;
+    }
+ for (let i = 0; i < array.length; i++) {
+    if (array[i] > f){
+        f = array[i];
+    }
+ }
+ return f;
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let f = 0;
+    let z = 0;
+    if (array == undefined || array.length == 0) {
+        return f = 0 ;
+    }
+  for ( let i = 0; i < array.length; i++) {
+      f += array[i];
+  }
+z = f/ array.length
+return z;
+  }
